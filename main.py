b28b5cf37c5e5ee570cccb01986bc4275ee5920c
Thang <t7dinhn@swisslog.com>
2021-05-01 17:37:13 +0200

    Added SHA Length attack class

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import codecs
+
 # ------------------------------------------------------
 # ------------------------------------------------------
 class Util():
@@ -88,7 +90,7 @@ class SHA1():
 
     # ------------------------------------------------------
     def getHash(self, key, text):
-        messageBinary = self.__convertMessageToBinary(str.encode(key) + str.encode(text))
+        messageBinary = self.__convertMessageToBinary(key.encode("utf-8", "ignore") + text.encode("utf-8", "ignore"))
         extendedMessageBinary = self.__extendMessageBinary(messageBinary)
         return self.__generateSHA1(extendedMessageBinary)
 
@@ -98,8 +100,16 @@ class SHA1():
         messageInBinary = ''
 
         # Convert each character to 8-bit binary. 
-        for char in message:
-            messageInBinary = messageInBinary + format(char, 'b').zfill(8)
+        index = 0
+        while (index < len(message)):
+            # messageInBinary = messageInBinary + format(message[index], 'b').zfill(8)
+            # index = index + 1
+            if (chr(message[index]) != '\\'):
+                messageInBinary = messageInBinary + format(message[index], 'b').zfill(8)
+                index = index + 1
+            else:
+                messageInBinary = messageInBinary +  bin(int(message[index:index+4].decode("utf-8").replace('\\x', ''), 16))[2:].zfill(8)
+                index = index + 4
 
         messageInBinaryLength = len(messageInBinary)
 
@@ -207,105 +217,114 @@ class SHA1_Length_Attack():
         self.util = Util();
 
     # ------------------------------------------------------
-    def attachSHA1(self, originText, originMac, appendText, originMessageLength):
+    # originMessageLength is (key + text)
+    def attackSHA1(self, originText, originMac, appendText, originMessageLength):
 
         originMacInBinary = bin(int(originMac, 16))[2:].zfill(8)
         h0 = originMacInBinary[0:32]
         h1 = originMacInBinary[32:64]
         h2 = originMacInBinary[64:96]
         h3 = originMacInBinary[96:128]
-        h4 = originMacInBinary[128:160]
+        h4 = originMacInBinary[128:160] 
 
-        h0_init = '01100111010001010010001100000001' 
-        h1_init = '11101111110011011010101110001001' 
-        h2_init = '10011000101110101101110011111110' 
-        h3_init = '00010000001100100101010001110110' 
-        h4_init = '11000011110100101110000111110000' 
+        # Calculate last chunk
+        totalNewLength = (int((originMessageLength * 8) / 512) + 1) * 512 + len(appendText) * 8 
+        lastChunk = self.__convertMessageToBinary(appendText.encode("utf-8", "ignore"), totalNewLength)
+        lastChunkExtended = self.__extendMessageBinary(lastChunk)
+        
+        newMac = (self.__generateLastChunkSHA1(lastChunkExtended,h0,h1,h2,h3,h4))
 
-        if (int(h0, 2) <= int(h0_init, 2)):
-            a = self.util.bitMINUS('1'+ h0, h0_init, 32)
-        else:
-            a = self.util.bitMINUS(h0, h0_init, 32)
+        # ---------------------------------------------------------
+        originMessageLengthInBinary = originMessageLength * 8
+        padding = '1'
 
-        if (int(h1, 2) <= int(h1_init, 2)):
-            b = self.util.bitMINUS('1'+ h1, h1_init, 32)
-        else:
-            b = self.util.bitMINUS(h1, h1_init, 32)
+        # Fill padding with bit "0", until there is 64 bits left (for message length)
+        while (originMessageLengthInBinary + len(padding)) % 512 != 448:
+            padding = padding + '0'
 
-        if (int(h2, 2) <= int(h2_init, 2)):
-            c = self.util.bitMINUS('1'+ h2, h2_init, 32)
-        else:
-            c = self.util.bitMINUS(h2, h2_init, 32)
+        # Last 64 bits is for message length
+        paddingAndLength = padding + '{0:064b}'.format(originMessageLengthInBinary)
 
-        if (int(h3, 2) <= int(h3_init, 2)):
-            d = self.util.bitMINUS('1'+ h3, h3_init, 32)
-        else:
-            d = self.util.bitMINUS(h3, h3_init, 32)
+        paddingAndLengthInHexStringFormat = ''
+        for i in range(0,len(paddingAndLength),8):
+            paddingAndLengthInHexStringFormat = paddingAndLengthInHexStringFormat + ('\\x' + '{:02x}'.format(int(paddingAndLength[i:i+8], 2)))
 
-        if (int(h4, 2) <= int(h4_init, 2)):
-            e = self.util.bitMINUS('1'+ h4, h4_init, 32)
-        else:
-            e = self.util.bitMINUS(h4, h4_init, 32)
-    
-        newMessage = ''
-        newMac = ''
+        newMessage = paddingAndLengthInHexStringFormat
         return [newMessage,newMac]
 
 
     # ------------------------------------------------------
-    def __convertMessageBinary(self, message):
-        messageBinary = ''
-        for char in message:
-            messageBinary = messageBinary + format(char, 'b').zfill(8)
+    def __convertMessageToBinary(self, message, newLength):
+        messageInBinary = ''
 
-        messageBinaryLength = len(messageBinary)
-        messageBinary = messageBinary + '1'
-        while len(messageBinary) % 512 != 448:
-            messageBinary = messageBinary + '0'
+        # Convert each character to 8-bit binary. 
+        index = 0
+        while (index < len(message)):
+            if (chr(message[index]) != '\\'):
+                messageInBinary = messageInBinary + format(message[index], 'b').zfill(8)
+                index = index + 1
+            else:
+                messageInBinary = messageInBinary +  bin(int(message[index:index+4].decode("utf-8").replace('\\x', ''), 16))[2:].zfill(8)
+                index = index + 4
 
-        return (messageBinary + '{0:064b}'.format(messageBinaryLength))
+        # Padding begins with a bit "1"
+        messageInBinary = messageInBinary + '1'
 
+        # Fill padding with bit "0", until there is 64 bits left (for message length)
+        while len(messageInBinary) % 512 != 448:
+            messageInBinary = messageInBinary + '0'
+
+        # Last 64 bits is for message length
+        messageInBinary = messageInBinary + '{0:064b}'.format(newLength)
+
+        return messageInBinary
 
     # ------------------------------------------------------
     def __extendMessageBinary(self, messageBinary):
-        extendedMessageBinary = ''
-        for index in range(80):
-            if index < 16:
-                messageBinary32BitChunk = messageBinary[(index+1)*32-32:(index+1)*32]
-                extendedMessageBinary = extendedMessageBinary + messageBinary32BitChunk
-            elif index >= 16:
-                w0 = extendedMessageBinary[(index-2)*32-32:(index-2)*32]
-                w1 = extendedMessageBinary[(index-7)*32-32:(index-7)*32]
-                w2 = extendedMessageBinary[(index-13)*32-32:(index-13)*32]
-                w3 = extendedMessageBinary[(index-15)*32-32:(index-15)*32]
 
-                w = ''
-                x1 = self.util.bitXOR(w0,w1,32) 
-                x2 = self.util.bitXOR(x1,w2,32)
-                x3 = self.util.bitXOR(x2,w3,32)
-                w = w + x3
+        result = ''
 
-                extendedMessageBinary = extendedMessageBinary + self.util.rotate(w,1)
+        # Divide the whole binary message into equal chunks of 512 bits length
+        for x in range(int(len(messageBinary) / 512)):
+            chunk = messageBinary[(x*512):(x+1)*512]
+            extendedChunkBinary = ''
+            for index in range(80):
+                if index < 16:
+                    extendedChunkBinary = extendedChunkBinary + chunk[(index*32):(index+1)*32]
+                elif index >= 16:
+                    w0 = extendedChunkBinary[(index-3)*32:(index-2)*32]
+                    w1 = extendedChunkBinary[(index-8)*32:(index-7)*32]
+                    w2 = extendedChunkBinary[(index-14)*32:(index-13)*32]
+                    w3 = extendedChunkBinary[(index-16)*32:(index-15)*32]
+
+                    w = ''
+                    x1 = self.util.bitXOR(w0,w1,32) 
+                    x2 = self.util.bitXOR(x1,w2,32)
+                    x3 = self.util.bitXOR(x2,w3,32)
+                    w = w + x3
+
+                    extendedChunkBinary = extendedChunkBinary + self.util.rotate(w,1)
                 
-        return extendedMessageBinary
+            result = result + extendedChunkBinary
 
+        return result
 
-    # ------------------------------------------------------
-    def __generateSHA1(self, extendedMessageBinary):
-        h0 = '01100111010001010010001100000001' # 67452301
-        h1 = '11101111110011011010101110001001' # EFCDAB89
-        h2 = '10011000101110101101110011111110' # 98BADCFE
-        h3 = '00010000001100100101010001110110' # 10325476
-        h4 = '11000011110100101110000111110000' # C3D2E1F0
-        
-        a = h0
-        b = h1
-        c = h2
-        d = h3
-        e = h4
+# ------------------------------------------------------
+    def __generateLastChunkSHA1(self, lastChunkOnly, last_h0, last_h1, last_h2, last_h3, last_h4):
+        h0 = last_h0 
+        h1 = last_h1
+        h2 = last_h2
+        h3 = last_h3
+        h4 = last_h4
+
+        a = last_h0
+        b = last_h1
+        c = last_h2
+        d = last_h3
+        e = last_h4
 
         for i in range(80):
-            w = extendedMessageBinary[(i+1)*32-32:(i+1)*32]
+            w = lastChunkOnly[(i*32):(i+1)*32]
             if (i <= 19): 
                 f = self.util.bitOR((self.util.bitAND(b,c,32)),(self.util.bitAND (self.util.bitNOT(b,32),d,32)),32)
                 k = '01011010100000100111100110011001' 
@@ -339,39 +358,46 @@ class SHA1_Length_Attack():
         return format(int(h0, 2), 'x') + format(int(h1, 2), 'x') + format(int(h2, 2), 'x') + format(int(h3, 2), 'x') + format(int(h4, 2), 'x')
 
 
-
-
-
-
-
-
-
 # ------------------------------------------------------
 # ------------------------------------------------------
 # ------------------------------------------------------
-
 def verifyMac(message, mac):
     key = 'secret'
     sha1 = SHA1()
     mySHA1 = sha1.getHash(key, message)
     return (mySHA1 == mac)
 
-print(verifyMac('musterman', 'c2cb7b4ef7d5e0468a28118d59f59504fa67cd06'))
-
-
+# ----------------------------------------
+# ----------------------------------------
 sha1 = SHA1()
-mySHA1 = sha1.getHash('key', 'c2cb7b4ef7d5e0468a28118d59f59504fa67cd06c2cb7b4ef7d5e0468a28118d59f59504fa67cd06c2cb7b4ef7d5e0468a28118d59f59504fa67cd06c2cb7b4ef7d5e0468a28118d59f59504fa67cd06c2cb7b4ef7d5e0468a28118d59f59504fa67cd06c2cb7b4ef7d5e0468a28118d59f59504fa67cd06c2cb7b4ef7d5e0468a28118d59f59504fa67cd06c2cb7b4ef7d5e0468a28118d59f59504fa67cd06c2cb7b4ef7d5e0468a28118d59f59504fa67cd06c2cb7b4ef7d5e0468a28118d59f59504fa67cd06c2cb7b4ef7d5e0468a28118d59f59504fa67cd06c2cb7b4ef7d5e0468a28118d59f59504fa67cd06c2cb7b4ef7d5e0468a28118d59f59504fa67cd06c2cb7b4ef7d5e0468a28118d59f59504fa67cd06c2cb7b4ef7d5e0468a28118d59f59504fa67cd06c2cb7b4ef7d5e0468a28118d59f59504fa67cd06c2cb7b4ef7d5e0468a28118d59f59504fa67cd06c2cb7b4ef7d5e0468a28118d59f59504fa67cd06c2cb7b4ef7d5e0468a28118d59f59504fa67cd06c2cb7b4ef7d5e0468a28118d59f59504fa67cd06c2cb7b4ef7d5e0468a28118d59f59504fa67cd06c2cb7b4ef7d5e0468a28118d59f59504fa67cd06c2cb7b4ef7d5e0468a28118d59f59504fa67cd06')
-print(mySHA1)
-
-
 sha1_attack = SHA1_Length_Attack()
-mySHA1 = sha1_attack.attachSHA1('musterman', 'c2cb7b4ef7d5e0468a28118d59f59504fa67cd06', 'max', 15)
+# ----------------------------------------
+
+print('----------------------------------------')
+print('Test 1:')
+originMAC1 = sha1.getHash('secret', 'a=1')
+print('Origin MAC: ' + originMAC1)
+# ----------------------------------------
+sha1_attack_new_data = sha1_attack.attackSHA1('a=1', originMAC1, '&b=3', 9)
+extendedString = sha1_attack_new_data[0]
+newMac = sha1_attack_new_data[1]
+print('New MAC: ' + newMac)
+print('Extended text: ' + extendedString)
+print('Verified result: ' + str(verifyMac('a=1' + extendedString + '&b=3', newMac)))
+
+# ----------------------------------------
+# ----------------------------------------
+print('----------------------------------------')
+print('Test 2:')
+originMAC2 = sha1.getHash('secret', 'IAmNotAdmin')
+print('Origin MAC: ' + originMAC2)
+# ----------------------------------------
+sha1_attack_new_data = sha1_attack.attackSHA1('IAmNotAdmin', originMAC2, 'YesIam', 17)
+extendedString = sha1_attack_new_data[0]
+newMac = sha1_attack_new_data[1]
+print('New MAC: ' + newMac)
+print('Extended text: ' + extendedString)
+print('Verified result: ' + str(verifyMac('IAmNotAdmin' + extendedString + 'YesIam', newMac)))
 
 
-# key = 'secret'
-# message = 'musterman'
-
-# sha1 = SHA1()
-# mySHA1 = sha1.getHash(key, message)
-# print('Key: "{key}" - Message: "{message}" : {value}'.format(key = key, message = message, value = mySHA1))
-
+#print(sha1.getHash('secret', 'I AmNotAdmin'))
